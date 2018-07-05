5a5df2ebe70923e21d9f2da1c97845b9c95182e3
Devesh Bajpai <BAJDE02@ca.com>
2018-07-05 11:40:22 -0700

    Number Mirror

---
+++ src/beginner/START01.java
@@ -0,0 +1,29 @@
+/**
+ * @url https://www.codechef.com/problems/START01
+ *
+ * @algorithm Standard output 
+ * 
+ * @problemname: Number Mirror
+ *
+ * @author Devesh Bajpai
+ */
+package beginner;
+
+import java.util.Scanner;
+
+/**
+ * @author bajde02
+ * 
+ */
+public class START01
+{
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt());
+	}
+
+}
